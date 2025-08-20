01e74763fe12bf2fb27719a80e55d79bd085578a
Ashley Yael Montiel Vargas <yaelmont@lcg.unam.mx>
2025-08-20 15:22:52 -0600

    Clase proteína y polimorfismos

--- source/ ejericio4_polimorfismo.py
+++ source/ ejericio4_polimorfismo.py
@@ -2,12 +2,15 @@ from ejercicio3_herencia import tRNA
 
 """La clase tRNA ya tiene el metodo de longitud heredado de Gen"""
 
-class Proteina(tRNA):
 
+class Proteina(tRNA):
     def __init__(self, nombre, secuencia_aa, organismo):
-        super().__init__(nombre, secuencia="", organismo, anticodon="", aminoacido="")
+        super().__init__(nombre, secuencia="", organismo=organismo, anticodon="", aminoacido="")
         self.secuencia_aa = secuencia_aa.upper()
 
-        self.secuencia_aa = secuencia_aa
-        
+    def longitud(self):
+        return len(self.secuencia_aa)
+    
+    def longitud_nt (self):
+        return len(self.secuencia_aa) * 3
 
